1215b791c7bf23df5bb43275fe309076b3e98dd5
AKORA <dragoncatm@gmail.com>
2023-01-31 16:30:24 +0100

    Exit process on failure

--- cli/src/commands/run.rs
+++ cli/src/commands/run.rs
@@ -25,14 +25,14 @@ pub fn run(
             if input.len() > 1 {
                 for i in 0..input.len() {
                     println!("Input {}:", i + 1);
-                    println!("{}", run_plugin(plugin, &input[i])?);
+                    run_plugin(plugin, &input[i]);
 
                     if i != input.len() - 1 {
                         print!("\n")
                     }
                 }
             } else {
-                print!("{}", run_plugin(plugin, &input[0])?);
+                run_plugin(plugin, &input[0]);
             }
         } else {
             let mut reader = BufReader::new(io::stdin());
@@ -42,7 +42,7 @@ pub fn run(
                 .read_to_string(&mut input_state)
                 .map_err(|e| BoopError::IoError(e))?;
 
-            print!("{}", run_plugin(plugin, &input_state)?);
+            run_plugin(plugin, &input_state);
         };
 
         io::stdout().flush().map_err(|e| BoopError::IoError(e))?;
@@ -54,9 +54,23 @@ pub fn run(
     }
 }
 
-fn run_plugin(p: &Box<dyn Plugin>, input: &str) -> Result<String, BoopError> {
-    p.run(input).map_err(|e| BoopError::PluginError {
+fn run_plugin(p: &Box<dyn Plugin>, input: &str) {
+    let result = p.run(input).map_err(|e| BoopError::PluginError {
         plugin_id: p.metadata().id,
         error: e,
-    })
+    });
+
+    match result {
+        Ok(new_state) => print!("{new_state}"),
+        Err(err) => {
+            match err {
+                BoopError::IoError(e) => eprintln!("IO Error: {}", e.to_string()),
+                BoopError::PluginError { plugin_id, error } => {
+                    eprintln!("Plugin {} threw: {}", plugin_id, error)
+                }
+                BoopError::PluginNotFound(e) => eprintln!("Unable to find plugin {e}"),
+            }
+            std::process::exit(1)
+        }
+    }
 }
